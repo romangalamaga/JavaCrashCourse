9a6219e48233382327383c6b39a712c2c25afd33
Roman Halamaha <romangalamaga@gmail.com>
2021-08-29 20:08:47 +0300

    implemented double int and multiply two numbers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import com.company.tasks.BasicProgram;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int res = BasicProgram.multiply(1, 2);
+        System.out.println(res);
+        // write your code here
     }
 }
---
+++ src/com/company/tasks/BasicProgram.java
@@ -0,0 +1,17 @@
+package com.company.tasks;
+
+public class BasicProgram {
+    /**
+     * https://www.codewars.com/kata/you-cant-code-under-pressure-number-1/
+     */
+    public static int doubleInteger(int i) {
+        return i * 2;
+    }
+
+    /**
+     * https://www.codewars.com/kata/function-3-multiplying-two-numbers/
+     */
+    public static int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+}
