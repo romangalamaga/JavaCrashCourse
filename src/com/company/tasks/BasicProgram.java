d2d12976079d33e79652f3050464bddbdfc9c480
Roman Halamaha <romangalamaga@gmail.com>
2021-09-04 17:52:11 +0300

    add crasshopper debug

--- src/com/company/tasks/BasicProgram.java
+++ src/com/company/tasks/BasicProgram.java
@@ -93,4 +93,21 @@ public class BasicProgram {
         return (int)min + "-" + (int)max;
     }
 
+    /**
+     * https://www.codewars.com/kata/grasshopper-debug
+     */
+    public static String weatherInfo(int temp) {
+        double c = convertToCelsius(temp);
+        if (c > 0){
+            return (c + " is above freezing temperature");
+        }else{
+            return (c + " is freezing temperature");
+        }
+    }
+
+    public static double convertToCelsius(int temperature) {
+        double celsius1 = (temperature - 32) * (5.0/9.0);
+        return celsius1;
+    }
+
 }
