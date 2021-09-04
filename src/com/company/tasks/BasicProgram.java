3e22356203f2dcc49bba06884c4d105c3c22b26d
Roman Halamaha <romangalamaga@gmail.com>
2021-09-04 16:12:22 +0300

    implemented volume of a cuboid and thinkful number drills pixelart planning

--- src/com/company/tasks/BasicProgram.java
+++ src/com/company/tasks/BasicProgram.java
@@ -14,4 +14,19 @@ public class BasicProgram {
     public static int multiply(int num1, int num2) {
         return num1 * num2;
     }
+
+    /**
+     * https://www.codewars.com/kata/volume-of-a-cuboid/
+     */
+    public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+        // Your code here
+        return length * width * height;
+    }
+
+    /**
+     * https://www.codewars.com/kata/thinkful-number-drills-pixelart-planning
+     */
+    public static boolean isDivisible(int wallLength, int pixelSize) {
+        return wallLength % pixelSize == 0;
+    }
 }
